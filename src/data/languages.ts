e2aff7b5cc333007e3d600c1bc566b90b26946d8
TheRivetedRose <redwoodrose707@gmail.com>
2026-02-17 21:59:15 -0800

    language section created and official branded icons added

---
+++ src/data/languages.ts
@@ -0,0 +1,17 @@
+import {
+  siPython,
+  siCplusplus,
+  siHtml5,
+  siJavascript,
+  siMysql,
+  siPostgresql,
+} from "simple-icons";
+
+export const languages = [
+  { name: "Python", icon: siPython },
+  { name: "C++", icon: siCplusplus },
+  { name: "HTML", icon: siHtml5 },
+  { name: "JavaScript", icon: siJavascript },
+  { name: "SQL", icon: siMysql },
+  { name: "PL/SQL", icon: siPostgresql },
+] as const;
---
+++ src/data/tools.ts
@@ -0,0 +1,19 @@
+import {
+  siUbuntu,
+  siVirtualbox,
+  siKalilinux,
+  siWireshark,
+  siGit,
+  siAstro,
+  siTailwindcss,
+} from "simple-icons";
+
+export const tools = [
+  { name: "Ubuntu", icon: siUbuntu },
+  { name: "VirtualBox", icon: siVirtualbox },
+  { name: "Kali Linux", icon: siKalilinux },
+  { name: "Wireshark", icon: siWireshark },
+  { name: "Git", icon: siGit },
+  { name: "Astro", icon: siAstro },
+  { name: "Tailwind CSS", icon: siTailwindcss },
+] as const;
